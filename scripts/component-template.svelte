60f24af3f0f343dfbbe9d8548167ed6fbf435b9c
Teddy Byron <ted@tedbyron.com>
2022-07-08 23:08:47 -0400

    add class attribute to icons

--- scripts/component-template.svelte
+++ scripts/component-template.svelte
@@ -2,11 +2,13 @@
     export let size = 24;
     export let color = "currentColor";
     export let strokeWidth = 2;
+    let className = ''
+    export { className as class }
 </script>
 
 <svg
     xmlns='http://www.w3.org/2000/svg'
-    class='icon icon-tabler icon-tabler-%%ORIGINAL_NAME%%'
+    class='icon icon-tabler icon-tabler-%%ORIGINAL_NAME%% {className}'
     width={size}
     height={size}
     viewBox='0 0 24 24'
